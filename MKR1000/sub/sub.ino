7c508379c990c57a655a94b92bb5575d55898441
lintangwisesa <lintangwisesa@ymail.com>
2021-06-21 09:56:36 +0700

    mkr1000 sub

---
+++ MKR1000/sub/sub.ino
@@ -0,0 +1,33 @@
+#include "credential.h"
+#include <ArduinoMqttClient.h>
+#include <WiFi101.h>
+
+WiFiClient wifiClient;
+MqttClient mqttClient(wifiClient);
+int count = 0;
+
+void setup() {
+    Serial.begin(9600);
+    while(WiFi.begin(WIFI_SSID, WIFI_PASS) != WL_CONNECTED){
+        Serial.print("."); 
+        delay(500);
+    }
+    Serial.println("Terhubung ke WiFi!");
+    mqttClient.setId(MQTT_CLID);
+    mqttClient.setUsernamePassword(MQTT_USER, MQTT_PASS);
+    if (!mqttClient.connect(MQTT_BROKER, MQTT_PORT)){
+        Serial.println("Gagal terhubung ke MQTT Cloud!");
+        Serial.println(mqttClient.connectError());
+    }
+    Serial.println("Terhubung ke MQTT Cloud!");
+    mqttClient.subscribe(MQTT_TOPIC);
+}
+
+void loop() {
+    if (mqttClient.parseMessage()){
+        while (mqttClient.available()){
+        Serial.print((char)mqttClient.read());
+        }
+        Serial.println();
+    }
+}
