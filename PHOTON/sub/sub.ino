18e90af7de3c9c839d2f9a1e7b4551cb4fde3cb1
lintangwisesa <lintangwisesa@ymail.com>
2021-07-04 15:50:30 +0700

    photon

---
+++ PHOTON/sub/sub.ino
@@ -0,0 +1,32 @@
+#include <MQTT.h>
+
+void callback(char* topic, byte* payload, unsigned int length);
+MQTT client("iot.reyax.com", 1883, callback);
+int count = 0;
+char str[8];
+
+void callback(char* topic, byte* payload, unsigned int length) {
+    char p[length + 1];
+    memcpy(p, payload, length);
+    p[length] = NULL;
+    Serial.println(p);
+}
+
+void setup() {
+    Serial.begin(9600);
+    WiFi.on();
+    WiFi.setCredentials("wifi_name", "wifi_pass");
+    WiFi.connect();
+    waitUntil(WiFi.ready);
+    while (WiFi.localIP() == IPAddress()) {
+        delay(10);
+    }
+    client.connect("mqtt_clientId", "mqtt_username", "mqtt_password");
+}
+
+void loop() {
+    if (client.isConnected()) {
+        client.loop();
+        client.subscribe("lintang");
+    }
+}
